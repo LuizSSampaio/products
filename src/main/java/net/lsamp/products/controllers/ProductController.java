e859f0cc17a20acee997fc0011fb6b28fe7af7fe
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2024-05-31 13:17:40 -0300

    feat: Added create product method

--- src/main/java/net/lsamp/products/controllers/ProductController.java
+++ src/main/java/net/lsamp/products/controllers/ProductController.java
@@ -1,11 +1,26 @@
 package net.lsamp.products.controllers;
 
+import jakarta.validation.Valid;
+import net.lsamp.products.dtos.ProductRecordDto;
+import net.lsamp.products.models.ProductModel;
 import net.lsamp.products.repositories.ProductRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
     @Autowired
     ProductRepository productRepository;
+
+    @PostMapping("/product")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        ProductModel productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 }

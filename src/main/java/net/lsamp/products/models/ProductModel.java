96cf410208b7c2035a7ded229859c597141c948d
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2024-06-07 15:22:10 -0300

    refactor: Removed unused setters

--- src/main/java/net/lsamp/products/models/ProductModel.java
+++ src/main/java/net/lsamp/products/models/ProductModel.java
@@ -26,23 +26,11 @@ public class ProductModel implements Serializable {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
     public int getStock() {
         return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
     }
 }
